c75e391db23f9881296a60e85a28261931c28e48
ryotarofr <ryoryo.fr0608@icloud.com>
2023-07-20 12:34:43 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import PostPreview from "../components/PostPreview";
 export const revalidate = 60
 
 const HomePage = () => {
-  const postMetadata = getPostMetadata();
-  const postPreviews = postMetadata.map((post) => (
-    <PostPreview key={post.slug} {...post} />
-  ));
+  // const postMetadata = getPostMetadata();
+  // const postPreviews = postMetadata.map((post) => (
+  //   <PostPreview key={post.slug} {...post} />
+  // ));
 
   return (
     // <div className="grid grid-cols-1 md:grid-cols-2 gap-4">{postPreviews}</div>
